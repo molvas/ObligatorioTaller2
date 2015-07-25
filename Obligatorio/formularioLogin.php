05e50f4c12430e912465a43381f212b71242b1dd
Fernando <Fernando@Fernando-PC>
2015-07-25 01:52:44 -0300

    se agrega login php

---
+++ Obligatorio/formularioLogin.php
@@ -0,0 +1,8 @@
+<?php
+echo 'esto va a hacer el login';
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
